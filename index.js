7463403eaa0c0f4abba3f27b91a6c708f2a3a0f0
Roberto <roberto.quadraccia90@gmail.com>
2023-09-12 11:10:16 +0200

    Notes view class created and tested, index file updated.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 console.log("The notes app is running")
 
 const NotesModel = require('./notesModel')
+const NotesView = require('./notesView')
 const model = new NotesModel()
-console.log(model.getNotes())
+model.addNote('Make dinner')
+model.addNote('Watch Apple event')
+const notesView = new NotesView(model)
+
+notesView.displayNotes()
+
+// console.log(model.getNotes())
